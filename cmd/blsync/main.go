7886197811965b025c3f9ba1757d4ea06b5dfef8
yolokos <mixan19931@gmail.com>
2026-02-10 19:08:08 +0200

    Revert "Initialize cleaning PoS"
    This reverts commit 1febe0573ff4a2ced8917b1c54bb05c98626ed1d.
    
    # Conflicts:
    #	consensus/pouw/engine.go

--- cmd/blsync/main.go
+++ cmd/blsync/main.go
@@ -22,6 +22,7 @@ import (
 	"os"
 	"slices"
 
+	"github.com/ethereum/go-ethereum/beacon/blsync"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/internal/debug"
 	"github.com/ethereum/go-ethereum/internal/flags"
